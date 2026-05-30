4db65bd1cf79586411f1f8e1ad03f9bfae9c7b11
Nidula03 <nidulanuwan@gmail.com>
2026-05-30 20:43:07 +0530

    feat: Enhance About section with additional personal insights and improved formatting

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -28,9 +28,10 @@ export default function AboutPage() {
                 <p>Hello! 👋😊</p>
                 <p>I'm a self-driven researcher with expertise in analytical chemistry and passionate about applying analytical techniques to solve complex problems with a focus on innovation and sustainability through multi-disciplinary knowledge in academic and industrial setting.</p>
               </div>
-              <Markdown>{`Advancing the traditional laboratory, I ♥️ go beyond the bench by implementing laboratory digitalization strategies to streamline workflows and integrating modern scientific techniques into conventional chemistry methods.
-
-Outside the lab, I am an avid hiker who finds inspiration in exploring challenging trails and the untamed beauty of nature. My experiences as a Boy Scout and Rover Scout, culminating in receiving both the Baden Powell Award and the President's Scout Award—the highest honors at the international and national levels of scouting have profoundly shaped my character. Today, I continue to explore, learn, and grow through scouting and outdoor experiences — building discipline, leadership, and a deeper connection with the world around me.`}</Markdown>
+              <Markdown>{`Advancing the traditional laboratory, I ♥️ go beyond the bench by implementing laboratory digitalization strategies to streamline workflows and integrating modern scientific techniques into conventional chemistry methods.`}</Markdown>
+              <div style={{ color: 'black' }}>
+                <Markdown>{`Outside the lab, I am an avid hiker who finds inspiration in exploring challenging trails and the untamed beauty of nature. My experiences as a Boy Scout and Rover Scout, culminating in receiving both the Baden Powell Award and the President's Scout Award—the highest honors at the international and national levels of scouting have profoundly shaped my character. Today, I continue to explore, learn, and grow through scouting and outdoor experiences — building discipline, leadership, and a deeper connection with the world around me.`}</Markdown>
+              </div>
             </div>
           </BlurFade>
         </div>
