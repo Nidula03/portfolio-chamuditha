faf832d01208283c2bee65979cb090db64e7de19
Nidula03 <nidulanuwan@gmail.com>
2026-05-30 20:50:11 +0530

    feat: Update About section with improved text alignment and formatting for better readability

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -24,12 +24,14 @@ export default function AboutPage() {
           />
           <BlurFade delay={BLUR_FADE_DELAY * 3}>
             <div className="prose max-w-full text-pretty leading-relaxed text-muted-foreground dark:prose-invert">
-              <div style={{ fontFamily: "'Times New Roman', Times, serif", color: 'black' }}>
+              <div style={{ textAlign: 'justify' }}>
                 <p>Hello! 👋😊</p>
                 <p>I'm a self-driven researcher with expertise in analytical chemistry and passionate about applying analytical techniques to solve complex problems with a focus on innovation and sustainability through multi-disciplinary knowledge in academic and industrial setting.</p>
               </div>
-              <Markdown>{`Advancing the traditional laboratory, I ♥️ go beyond the bench by implementing laboratory digitalization strategies to streamline workflows and integrating modern scientific techniques into conventional chemistry methods.`}</Markdown>
-              <div style={{ color: 'black' }}>
+              <div style={{ textAlign: 'justify' }}>
+                <Markdown>{`Advancing the traditional laboratory, I ♥️ go beyond the bench by implementing laboratory digitalization strategies to streamline workflows and integrating modern scientific techniques into conventional chemistry methods.`}</Markdown>
+              </div>
+              <div style={{ textAlign: 'justify' }}>
                 <Markdown>{`Outside the lab, I am an avid hiker who finds inspiration in exploring challenging trails and the untamed beauty of nature. My experiences as a Boy Scout and Rover Scout, culminating in receiving both the Baden Powell Award and the President's Scout Award—the highest honors at the international and national levels of scouting have profoundly shaped my character. Today, I continue to explore, learn, and grow through scouting and outdoor experiences — building discipline, leadership, and a deeper connection with the world around me.`}</Markdown>
               </div>
             </div>
